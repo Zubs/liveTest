29051f37e97128b88e137f24da1d938e81f48387
Zubs <zubairidrisaweda@gmail.com>
2021-07-17 20:09:41 +0100

    Everything works

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const io = require('socket.io')(8090)
+const httpServer = require("http").createServer();
+const io = require("socket.io")(httpServer, {
+    cors: {
+        origin: "http://localhost:8080",
+    },
+});
 
-io.on('connection', (socket) => {
-    console.log(socket.id)
-})
+io.on("connection", (socket) => {
+  console.log(socket.id)
+});
+
+httpServer.listen(3000);
